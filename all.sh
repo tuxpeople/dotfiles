1e6d8ab831d3a20105c3207603509f6a52cb3be5
tuxpeople <thomas@tuxpeople.org>
2020-03-10 12:44:58 +0100

    Update all.sh

--- all.sh
+++ all.sh
@@ -2,3 +2,11 @@ cd ~/git/dotfiles
 ./brew.sh
 source bootstrap.sh
 ./.macos
+
+# Checkout all my github repos
+mkdir ~/git
+cd ~/git
+for i in $(wget -q -O- "https://github.com/tuxpeople?tab=repositories" | grep tuxpeople | grep codeRepository | cut -d'"' -f2 | cut -d'/' -f3)
+do
+  git clone git@github.com:tuxpeople/$i.git
+done
