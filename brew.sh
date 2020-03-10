e0aceba321c8d2bb84511e532c2e8f15dd95865c
tuxpeople <thomas@tuxpeople.org>
2020-03-10 11:24:09 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -24,34 +24,12 @@ ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
+
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed
-# Install a modern version of Bash.
-#brew install bash
-#brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-#if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
- # echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-#  chsh -s "${BREW_PREFIX}/bin/bash";
-#fi;
-
-# Install `wget`
-brew install wget
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install vim
-brew install grep
-brew install openssh
-brew install screen
 
 # Install other stuff
-#brew bundle install
+brew bundle install
 
 # Remove outdated versions from the cellar.
 brew cleanup
