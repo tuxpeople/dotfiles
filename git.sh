3d11ce5b7f3ebcd43a6237e0a6bdec32861b1637
Thomas Deutsch <thomas@tuxpeople.org>
2020-09-23 08:29:56 +0200

    changes

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Checkout all my github repos
+mkdir ~/git
+cd ~/git
+for i in $(wget -q -O- "https://github.com/tuxpeople?tab=repositories" | grep tuxpeople | grep codeRepository | cut -d'"' -f2 | cut -d'/' -f3)
+do
+  git clone git@github.com:tuxpeople/$i.git
+done
